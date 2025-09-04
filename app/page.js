02898d6b0f1356dc12d4dbb81e1382b997dc259c
230631040-maker <230631040@vfu.bg>
2025-09-04 20:51:39 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,16 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    // Централна част със светъл фон и само логото
     <div className="flex flex-col items-center justify-center min-h-[60vh]">
-      {/* Ако логото ти е PNG, смени src="/logo.svg" на src="/logo.png" */}
-      <Image
-        src="/logo.svg"
-        alt="Dr.Kostadinov Logo"
-        width={320}
-        height={320}
-        priority
-      />
+      {/* смени на /logo.png ако ползваш PNG */}
+      <Image src="/logo.svg" alt="Dr.Kostadinov Logo" width={320} height={320} priority />
     </div>
   );
 }
