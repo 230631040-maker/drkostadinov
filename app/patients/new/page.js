6f67d38bb6977384fd40f784b77da0ab3dd457db
230631040-maker <230631040@vfu.bg>
2025-09-04 21:20:00 +0300

    Update page.js

--- app/patients/new/page.js
+++ app/patients/new/page.js
@@ -6,8 +6,8 @@ export default function NewPatient() {
   const router = useRouter();
   const [form, setForm] = useState({
     name: "",
-    egn: "",
     birth: "",
+    firstVisit: "",
     phone: "",
     email: "",
   });
@@ -15,14 +15,13 @@ export default function NewPatient() {
   function handleSubmit(e) {
     e.preventDefault();
     console.log("Patient saved:", form);
-    // по-късно ще запишем в база (Supabase)
     alert("Пациентът е добавен успешно!");
     router.push("/patients");
   }
 
   return (
     <section className="grid gap-6 max-w-lg">
-      <h2 className="text-3xl font-bold">➕ Нов пациент</h2>
+      <h2 className="text-3xl font-bold text-[#212845]">➕ Нов пациент</h2>
 
       <form onSubmit={handleSubmit} className="grid gap-3">
         <input
@@ -31,24 +30,30 @@ export default function NewPatient() {
           value={form.name}
           onChange={(e) => setForm({ ...form, name: e.target.value })}
         />
-        <input
-          className="border rounded p-2"
-          placeholder="ЕГН"
-          value={form.egn}
-          onChange={(e) => setForm({ ...form, egn: e.target.value })}
-        />
+
         <input
           type="date"
           className="border rounded p-2"
           value={form.birth}
           onChange={(e) => setForm({ ...form, birth: e.target.value })}
+          placeholder="Рожд. дата"
         />
+
+        <input
+          type="date"
+          className="border rounded p-2"
+          value={form.firstVisit}
+          onChange={(e) => setForm({ ...form, firstVisit: e.target.value })}
+          placeholder="Дата на първо посещение"
+        />
+
         <input
           className="border rounded p-2"
           placeholder="Телефон"
           value={form.phone}
           onChange={(e) => setForm({ ...form, phone: e.target.value })}
         />
+
         <input
           type="email"
           className="border rounded p-2"
@@ -56,7 +61,8 @@ export default function NewPatient() {
           value={form.email}
           onChange={(e) => setForm({ ...form, email: e.target.value })}
         />
-        <button className="bg-blue-600 text-white px-4 py-2 rounded">
+
+        <button className="bg-[#D6628D] hover:bg-[#b84d6f] text-white px-4 py-2 rounded transition">
           Запази пациента
         </button>
       </form>
