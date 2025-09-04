4a902aa2411f581edf3c6156a924091d566fa5d4
230631040-maker <230631040@vfu.bg>
2025-09-04 16:58:09 +0300

    Delete app/about/page.js

--- app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <section className="grid gap-4">
-      <h2 className="text-3xl font-bold">About</h2>
-      <p className="text-gray-700">Тук опиши кой си, мисия, сертификати, опит.</p>
-    </section>
-  );
-}
