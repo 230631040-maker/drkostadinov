2ed9f0dd3b0ac0b45e1a3930021e8743b2eb262a
230631040-maker <230631040@vfu.bg>
2025-09-04 15:50:37 +0300

    add: home page in app/page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
+      <h1>Hello, world! 🚀</h1>
+      <p>Началната страница вече работи!</p>
+    </main>
+  );
+}
