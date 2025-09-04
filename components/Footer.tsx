e3f5c39add1dfcfb40d8785bb03d1c1979fe34e8
230631040-maker <230631040@vfu.bg>
2025-09-04 16:46:14 +0300

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 bg-gray-900 text-gray-200 text-center">
+      <p>© {new Date().getFullYear()} Perfectno. All rights reserved.</p>
+    </footer>
+  );
+}
