680f47e7d04c2313dcf2c573f304a684da8610e6
230631040-maker <230631040@vfu.bg>
2025-09-04 20:32:13 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="p-4 bg-gray-900 text-gray-200 text-center">
-      <p>© {new Date().getFullYear()} Perfectno. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Dr.Kostadinov. All rights reserved.</p>
     </footer>
   );
 }
