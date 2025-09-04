787e5f4013264ca6e7d927991e4bd30f6b9bb382
230631040-maker <230631040@vfu.bg>
2025-09-04 22:07:58 +0300

    Update Container.tsx

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="max-w-5xl mx-auto px-4">{children}</div>;
+  return <div className="w-full px-6 lg:px-8">{children}</div>;
 }
