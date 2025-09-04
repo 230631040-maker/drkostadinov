0fcf92c35330913570b9adf9ec1d946d327a6ce5
230631040-maker <230631040@vfu.bg>
2025-09-04 21:05:59 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
-import Image from "next/image";
+"use client";
+import { Brain } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[60vh]">
-      {/* смени на /logo.png ако ползваш PNG */}
-      <Image src="/logo.svg" alt="Dr.Kostadinov Logo" width={320} height={320} priority />
+    <div className="flex flex-col items-center justify-center min-h-[60vh] text-center space-y-6">
+      {/* Икона на мозък */}
+      <div className="p-6 rounded-full bg-[#D6ED17]/20">
+        <Brain size={80} className="text-[#212845]" />
+      </div>
+
+      {/* Приветствено съобщение */}
+      <h1 className="text-4xl font-extrabold text-[#212845]">
+        Добре дошли при <span className="text-[#D6628D]">Dr.Kostadinov</span>
+      </h1>
+      <p className="text-lg text-gray-700 max-w-2xl">
+        Вашето приложение за психично здраве, диагнози и изследвания.
+      </p>
     </div>
   );
 }
